b0bd87c08e23dbcc5755139e611aaf321ee5411c
samm81 <samwmaynard@gmail.com>
2014-02-28 20:05:58 -0500

    graphs working!

--- src/Commodity.java
+++ src/Commodity.java
@@ -5,7 +5,7 @@ import java.util.LinkedList;
 import java.util.Random;
 
 public abstract class Commodity {
-
+	
 	LinkedList<Transaction> transactions;
 	Hashtable<Class<?>, Graph> graphs;
 	
@@ -25,7 +25,7 @@ public abstract class Commodity {
 	}
 	
 	public void addTransaction(Transaction transaction) {
-		if(!isOrderedProperly(transaction)){
+		if(!isOrderedProperly(transaction)) {
 			transaction = transaction.getReversedTransaction();
 		}
 		transactions.add(transaction);
@@ -38,39 +38,39 @@ public abstract class Commodity {
 			graphs.get(tradeCommodity.getClass()).addDataPoint(transaction.getRatio());
 		}
 	}
-
+	
 	private Color randomColor() {
 		Random r = new Random();
 		return new Color(r.nextFloat(), r.nextFloat(), r.nextFloat());
 	}
-
+	
 	private boolean isOrderedProperly(Transaction transaction) {
 		return transaction.getCommodity1().getClass().equals(this.getClass());
 	}
-
+	
 	public class Graph {
 		
 		LinkedList<Double> datum;
 		
-		double maxData = 0;
+		double maxData = 10;
 		
 		Color color;
 		
-		final int r = 6;
+		final int r = 7;
 		
 		public Graph(int size, Color color) {
 			this.color = color;
 			
 			datum = new LinkedList<Double>();
-			for(int i=0;i<size;i++){
+			for(int i = 0; i < size; i++) {
 				datum.add(0d);
 			}
-			findMaxData();
+			//findMaxData();
 		}
 		
 		private void findMaxData() {
-			for(double data : datum){
-				if(data > maxData){
+			for(double data : datum) {
+				if(data > maxData) {
 					maxData = data;
 				}
 			}
@@ -78,28 +78,36 @@ public abstract class Commodity {
 		
 		public void addDataPoint(double dataPoint) {
 			double first = datum.removeFirst();
-			if(first == maxData) {
-				maxData = 0;
-				findMaxData();
-			}
+			/*
+			 * if(first == maxData) {
+			 * maxData = 0;
+			 * findMaxData();
+			 * }
+			 */
 			datum.add(dataPoint);
-			if(dataPoint > maxData) {
-				maxData = dataPoint;
-			}
+			/*
+			 * if(dataPoint > maxData) {
+			 * maxData = dataPoint;
+			 * }
+			 */
 		}
 		
 		public void drawSelf(int x, int y, int width, int height, Graphics2D g) {
-			double dx = (double) (width - x) / (double) datum.size();
+			g.setColor(color);
+			
+			double dx = (double) width / (double) (datum.size() - 1);
 			double datax = x;
 			double datay = y;
-			g.setColor(color);
-			for(double data : datum){
-				datay = y + height - ((double) height * data / maxData);
-				g.fillOval((int)(datax - r/2d), (int)(datay - r/2d), r, r);
+			
+			for(double data : datum) {
+				if(data != 0) {
+					datay = y + height - ((double) height * ((double) data / maxData));
+					g.fillOval((int) (datax - r / 2d), (int) (datay - r / 2d), r, r);
+				}
 				
 				datax += dx;
 			}
 		}
 	}
-
+	
 }
--- src/MarketCanvas.java
+++ src/MarketCanvas.java
@@ -5,21 +5,21 @@ import java.awt.Graphics2D;
 
 @SuppressWarnings("serial")
 public class MarketCanvas extends DoubleBufferedCanvas {
-
+	
 	MarketSimulation sim;
-
+	
 	public MarketCanvas(int fps, MarketSimulation sim) {
 		super(fps);
 		this.sim = sim;
 	}
-
+	
 	@Override
 	void draw(Graphics graphics) {
 		Graphics2D g = (Graphics2D) graphics;
 		g.setColor(Color.BLACK);
-
+		
 		int i = 0;
-		for (Commodity commodity : sim.getCommodities()) {
+		for(Commodity commodity : sim.getCommodities()) {
 			int x = 10 + (this.getWidth() / 2 * (i % 2));
 			int y = 60 + 210 * (i / 2);
 			int width = this.getWidth() / 2 - 20;
@@ -27,17 +27,17 @@ public class MarketCanvas extends DoubleBufferedCanvas {
 			drawGraph(x, y, width, height, commodity, g);
 			i++;
 		}
-
+		
 	}
-
+	
 	private void drawGraph(int x, int y, int width, int height, Commodity commodity, Graphics2D g) {
 		g.setColor(Color.BLACK);
 		g.fillRoundRect(x, y, width, height, 50, 50);
 		int offset = 3;
 		g.setColor(Color.WHITE);
-		//g.fillRoundRect(x + offset, y + offset, width - offset, height - offset, 50, 50); // cool effect
+		// g.fillRoundRect(x + offset, y + offset, width - offset, height - offset, 50, 50); // cool effect
 		g.fillRoundRect(x + offset, y + offset, width - offset * 2, height - offset * 2, 50, 50);
-
+		
 		int titlex = x + 30;
 		int titley = y + 30;
 		String name = commodity.getClass().getSimpleName();
@@ -45,15 +45,30 @@ public class MarketCanvas extends DoubleBufferedCanvas {
 		g.setColor(Color.BLACK);
 		g.drawString(name, titlex, titley);
 		
-		for(Commodity.Graph graph : commodity.getGraphs().values()){
-			graph.drawSelf(x + 30, y + 60, width - 40, height - 100, g);
+		int graphx = x + 30;
+		int graphy = y + 50;
+		int graphWidth = width - 50;
+		int graphHeight = height - 70;
+		
+		g.setColor(new Color(.1f, .1f, .1f));
+		double dy = graphHeight / 7d;
+		for(int i = 0; i <= 7; i++) {
+			int x1 = graphx;
+			int y1 = graphy + (int) (i * dy);
+			int x2 = graphx + graphWidth;
+			int y2 = y1;
+			g.drawLine(x1, y1, x2, y2);
+		}
+		
+		for(Commodity.Graph graph : commodity.getGraphs().values()) {
+			graph.drawSelf(graphx, graphy, graphWidth, graphHeight, g);
 		}
 	}
-
+	
 	@Override
 	protected void updateVars() {
 		// TODO Auto-generated method stub
-
+		
 	}
-
+	
 }
