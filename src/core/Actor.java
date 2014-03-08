1e2abd495a5695c191f1fd48114ffabd2d4b6980
samm81 <samwmaynard@gmail.com>
2014-03-07 21:38:26 -0500

    Reworked actor

--- src/core/Actor.java
+++ src/core/Actor.java
@@ -1,99 +1,32 @@
 package core;
+
+import java.util.LinkedList;
 import java.util.concurrent.LinkedBlockingQueue;
-//TODO: On a setInterval, check the states of transactions so they can update their respective tracks.
 
-//Every actor needs to have a transaction data structure as well,
-//so they can have a history of previous transactions
-//Also has the added benefit of letting me see when Transaction states have changed so we can update the GUI.
+import core.commodities.Commodity;
+
 public class Actor {
-	public final String name;
+	
+	protected LinkedList<Commodity> commodities;
 	protected LinkedBlockingQueue<Transaction> transactions;
-	protected LinkedBlockingQueue<Transaction> globalTrans;//This gets synced with the global transactions
-	protected LinkedBlockingQueue<Transaction> offers;
-	protected volatile boolean transactionProcessed = false;
-	public Actor(String sentName, LinkedBlockingQueue <Transaction> queue){
-		name = sentName;
-		globalTrans = queue;
+	
+	public Actor(LinkedList<Commodity> commodities, LinkedBlockingQueue<Transaction> transaction) {
+		this.commodities = commodities;
+		this.transactions = transaction;
 	}
-
-	public String getName(){
-		return name;
-	}
-
+	
 	public void reevaluateWeights() {
 		// TODO Auto-generated method stub
-		//Do evaluation stuff here then
-		//Check if any of your transactions have been processed
-		if(this.transactionProcessed){
-			for(Transaction t : this.offers){
-				switch(t.getState()){
-				case Transaction.STATE_ACCEPTED:
-					System.out.println("Transaction" + t.getID() +" has been accepted.");
-					break;
-				case Transaction.STATE_DECLINE:
-					System.out.println("Transaction" + t.getID() +" has been declined by recieving actor.");
-					removeFromOffers(t);
-					break;
-				case Transaction.STATE_INVALID:
-					System.out.println("Transaction" + t.getID() +" is unacceptable.");
-					removeFromOffers(t);
-					break;
-				default:break;
-				}
-			}
-			this.transactionProcessed = false;
-		}
-
 	}
-
+	
 	public Transaction getBestOffer() {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
+	
 	public void evaluateMarket() {
 		// TODO Auto-generated method stub
-
+		
 	}
-	//If they accept an offer we need to send a transaction to update the resource counts of both
-	//Actors as well update the global market that a trade happened at a new price.
-	public void acceptOffer(Transaction accept){
-		try{
-			globalTrans.put(accept);
-			accept.setState(Transaction.STATE_SUBMITTED);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}finally{}
-	}
-	//If the Actor declines, we set the state to declined and remove it from their list of offers
-	//but leave it in their transaction history
-	public void declineOffer(Transaction decline){
-		try{
-			removeFromOffers(decline);
-		}finally{
-			decline.setState(Transaction.STATE_DECLINE);
-		}
-	}
-	/**
-	 * Remove from Offer List
-	 * @param offer- Transaction to remove from queue
-	 */
-	private void removeFromOffers(Transaction offer) {
-		// TODO Auto-generated method stub
-		for(Transaction o : offers){
-			if(o.equals(offer)){
-				offers.remove(o);
-				break;
-			}
-		}
-	}
-
-	/**
-	 * Quite a useless method. 
-	 * Just a stub, for if we need some exit code to occur for deferring like moving it to the back of the queue.
-	 * @return Nothing for now.
-	 */
-	public void deferOffer(){}
-
+	
 }
--- src/core/MarketSimulation.java
+++ src/core/MarketSimulation.java
@@ -1,4 +1,5 @@
 package core;
+
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Random;
@@ -7,55 +8,55 @@ import java.util.concurrent.LinkedBlockingQueue;
 import core.commodities.Commodity;
 
 public class MarketSimulation extends Simulation {
-
+	
 	protected HashSet<Actor> actors;
 	protected LinkedList<Commodity> commodities;
-	protected LinkedBlockingQueue<Transaction> globalTrans;
+	protected LinkedBlockingQueue<Transaction> transactions;
+	
 	public MarketSimulation(double dt) {
 		super(dt);
 		actors = new HashSet<Actor>();
 		commodities = new LinkedList<Commodity>();
-		globalTrans = new LinkedBlockingQueue<Transaction>();
+		transactions = new LinkedBlockingQueue<Transaction>();
 	}
 	
 	public LinkedBlockingQueue<Transaction> getTrans() {
 		// TODO Auto-generated method stub
 		return this.getTrans();
 	}
+	
 	public HashSet<Actor> getActors() {
 		return this.actors;
 	}
-
+	
 	public LinkedList<Commodity> getCommodities() {
 		return this.commodities;
 	}
-
-
-
+	
 	public void addActor(Actor actor) {
 		this.actors.add(actor);
 	}
-
+	
 	public void addCommodity(Commodity commodity) {
 		this.commodities.add(commodity);
 	}
-
+	
 	@Override
 	protected void initialize() {
 		// TODO Auto-generated method stub
-
+		
 	}
-
+	
 	@Override
 	protected void tick() {
 		for(Actor actor : this.actors) {
 			actor.reevaluateWeights();
 		}
-
+		
 		for(Actor actor : this.actors) {
 			actor.evaluateMarket();
 		}
-
+		
 		Random r = new Random();
 		int commodity1 = r.nextInt(this.commodities.size());
 		int commodity2 = 0;
@@ -63,10 +64,7 @@ public class MarketSimulation extends Simulation {
 			commodity2 = r.nextInt(this.commodities.size());
 		} while(commodity2 == commodity1);
 		this.commodities.get(commodity1).addTransaction(new Transaction(r.nextInt(9) + 1, this.commodities.get(commodity1), r.nextInt(9) + 1, commodities.get(commodity2)));
-
+		
 	}
-
-
 	
-
 }
