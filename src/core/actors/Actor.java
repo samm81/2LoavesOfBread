a0e036832a4245e6b27f45553cbc8e9dd71d14de
Patrick Shan <patrickcshan@gmail.com>
2014-04-14 01:00:21 -0400

    2 Watermelon for 1 Fish 3 Watermelon for 3 Fish 5 Watermelon for 6 Fish Watermelons OP in this update: made it so actors may not trade a commodity for the same commodity and cannot trade using a value of 0

--- src/core/actors/Actor.java
+++ src/core/actors/Actor.java
@@ -140,7 +140,7 @@ public void initialize(LinkedList<Commodity> commodities, LinkedBlockingQueue<Tr
 	 * @param t the transaction to be performed
 	 */
 	public void acceptTransaction(Transaction t) {
-		if(this.volumes.get(t.commodity1) - t.volume1 > 0) {
+		if(this.volumes.get(t.commodity1) - t.volume1 > 0 && t.commodity1 != t.commodity2 && t.volume1 != 0 && t.volume2 != 0) {
 			this.volumes.put(t.getCommodity1(), new Integer((int) (this.volumes.get(t.getCommodity1()).intValue() + t.getVolume1())));
 			this.volumes.put(t.getCommodity2(), new Integer((int) (this.volumes.get(t.getCommodity2()).intValue() + t.getVolume2())));
 			System.out.println("Trade made " + t.volume1 + " " + t.commodity1.name() + " for " + t.volume2 + " " + t.commodity2.name());
